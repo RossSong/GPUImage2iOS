1a26467869fd6b1a35233c260c42fe09c09de0f8
hgsong <hgsong@wantedlab.com>
2017-08-24 18:24:55 +0900

    first commit

---
+++ DerivedData/GPUImage/Build/Intermediates/GPUImage.build/Debug-iphoneos/GPUImageTests_iOS.build/DerivedSources/GPUImageTests_iOS_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char GPUImageTests_iOSVersionString[];
+ extern const double GPUImageTests_iOSVersionNumber;
+
+ const unsigned char GPUImageTests_iOSVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:GPUImageTests_iOS  PROJECT:GPUImage-1" "\n";
+ const double GPUImageTests_iOSVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ DerivedData/GPUImage/Build/Intermediates/GPUImage.build/Debug-iphoneos/GPUImage_iOS.build/Objects-normal/arm64/GPUImage_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/ross/Downloads/GPUImage2-master/framework/DerivedData/GPUImage/Build/Intermediates/GPUImage.build/Debug-iphoneos/GPUImage_iOS.build/DerivedSources/GPUImage_vers.c
